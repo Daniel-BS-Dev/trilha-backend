d74c5e0c657aec1463bea0e600f3914682ee0182
Daniel Silva <daniel.silva@onlineapp.com.br>
2022-04-12 08:36:25 -0300

    string

---
+++ trilha backend/string e exprexões regulares/ConsoleApp1/Extrato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Extrato
+    {
+        public string Url { get; }
+
+        public Extrato(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio.", nameof(url));
+            }
+        }
+    }
+}
---
+++ trilha backend/string e exprexões regulares/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string url = "pagamento?argumentos";
+
+            int indiceInterrogacao = url.IndexOf("?"); // pega o index que esta o meu "?"
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            Console.WriteLine(url.Substring(7)); // recorta aparti do "?"
+            Console.WriteLine(url.Substring(indiceInterrogacao + 1)); // recotando da minha interrogação por diante
+
+            try
+            {
+                Extrato e = new Extrato("");
+            }catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
